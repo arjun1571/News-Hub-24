56459f1f88ba04addbc82770db0e33655bbd0fe4
Arjun Das <arjundasback515@gmail.com>
2022-09-02 22:48:11 +0600

    category section added using fetch api

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("app.js connected");
+const newsData = () =>{
+    fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then(res => res.json())
+    .then(data=>displayCategory(data))
+
+}
+
+const displayCategory = (data) =>{
+    console.log(data);
+    const categoryList=document.getElementById("category-list");
+    data.data.news_category.forEach((datas) => {
+        const {category_name}=datas
+        
+        const createDiv=document.createElement("div")
+        createDiv.classList.add("cata-list-style")
+        createDiv.innerText=`
+        ${category_name}
+        `
+        categoryList.appendChild(createDiv);
+    });
+}
+newsData();
