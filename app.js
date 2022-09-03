1a4fa7a1f27d369a74afe4a45a2b5825f761a8bd
Arjun Das <arjundasback515@gmail.com>
2022-09-03 13:58:04 +0600

    blog section created

--- app.js
+++ app.js
@@ -88,6 +88,7 @@ const newsModal = (data) =>{
     const setModal = document.getElementById("set-modal");
     setModal.innerHTML=""
     data.forEach((datas) => {
+        const {title,thumbnail_url,details}=datas;
         const createDiv=document.createElement("div")
         createDiv.innerHTML=`
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
@@ -95,12 +96,12 @@ const newsModal = (data) =>{
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">${datas.title}</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                        <img class="mx-auto d-block" src="${datas.thumbnail_url}" style="width:80%" alt="">
-                        <p>${datas.details}</p>
+                        <img class="mx-auto d-block" src="${thumbnail_url}" style="width:80%" alt="">
+                        <p>${details}</p>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
