14ef88a87e409766e95c37913a0caa278eafab57
Arjun Das <arjundasback515@gmail.com>
2022-09-03 20:05:07 +0600

    text elipcies added

--- app.js
+++ app.js
@@ -45,22 +45,25 @@ const displayNews = (data) =>{
     const newsPost = document.getElementById("news-detail");
     newsPost.innerHTML=""
     data.data.forEach((datas) => {
+        
         document.getElementById("spiner").style.display="none";
         const {title,details,image_url,author,_id,total_view}=datas;
+        console.log(details);
         const createDiv=document.createElement("div")
         createDiv.innerHTML=`
         <div class="card mb-3" >
             <div class="row g-0">
                 <div class="col-md-4">
-                    <img src="${datas.image_url ? datas.image_url:"N/A" }" class="img-fluid rounded-start" style="height:100%" alt="...">
+                    <img src="${image_url ? image_url:"N/A" }" class="img-fluid rounded-start" style="height:100%" alt="...">
                     </div>
                     <div class="col-md-8">
                     <div class="card-body">
                     <h5 class="card-text">${title}</h5>
-                    <p>${details.slice(0,200)}</p>
+                    <p class="text-truncate">${details}</p>
                     <div class="d-flex p-2">
-                    <img src="${image_url}" style="height:30px" class="rounded-circle p-2" alt=""  >
-                    <p>${author.name}</p>
+                    <div class="d-flex  align-items-center">
+                    <img src="${author.img}" style="height:60px" class="rounded-circle p-2" alt=""  >
+                    <p>${author.name}</p></div>
                     </div>
                     
                     <button type="button" onclick="catagoryNews('${_id}')"   class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -76,7 +79,6 @@ const displayNews = (data) =>{
     });
 }
 const catagoryNews = (id) =>{
-    console.log(id);
     fetch(`https://openapi.programming-hero.com/api/news/${id}`)
     .then(res=>res.json())
     .then(data=>newsModal(data.data))
@@ -92,25 +94,26 @@ const newsModal = (data) =>{
         const {title,thumbnail_url,details}=datas;
         const createDiv=document.createElement("div")
         createDiv.innerHTML=`
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                        <img class="mx-auto d-block" src="${thumbnail_url}" style="width:80%" alt="">
-                        <p>${details}</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                    <div class="modal-body">
+                    <img class="mx-auto d-block" src="${thumbnail_url}" style="width:80%" alt="">
+                    <p>${details}</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
                     </div>
                 </div>
             </div>
+            </div>
+
     
         `
         setModal.appendChild(createDiv); 
